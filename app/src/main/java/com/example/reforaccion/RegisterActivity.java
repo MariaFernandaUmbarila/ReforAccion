92116748f7a42c03b4ffb45ccc0eaacbf6b82032
MariaFernandaUmbarila <fernandaumbarila@gmail.com>
2024-07-09 11:36:16 -0500

    Register page improved, added basic functions for buttons

--- app/src/main/java/com/example/reforaccion/RegisterActivity.java
+++ app/src/main/java/com/example/reforaccion/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.example.reforaccion;
 
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,17 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.register_activity);
+        registerUserButtonListener();
+        returnButtonListener();
+    }
+
+    private void registerUserButtonListener(){
+        Button registerUserButton = findViewById(R.id.registerUserButton);
+        registerUserButton.setOnClickListener(v -> System.out.println("Registrando usuario"));
+    }
+
+    private void returnButtonListener(){
+        ImageButton returnButton = findViewById(R.id.returnRegisterButton);
+        returnButton.setOnClickListener(v -> finish());
     }
 }
