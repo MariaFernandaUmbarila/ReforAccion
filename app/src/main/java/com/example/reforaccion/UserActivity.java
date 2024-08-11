95637fd90d291ed53e6f307756f6188c3c24e51a
MariaFernandaUmbarila <fernandaumbarila@gmail.com>
2024-08-11 14:03:40 -0500

    User info completed

--- app/src/main/java/com/example/reforaccion/UserActivity.java
+++ app/src/main/java/com/example/reforaccion/UserActivity.java
@@ -4,19 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.reforaccion.models.UserApplication;
 
 
 public class UserActivity extends AppCompatActivity {
+
+    UserApplication currentUser;
+    TextView usernameText;
+    TextView cellphoneText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.user_activity);
+        initiateVars();
+        renderUsernameInfo();
         logoutButtonListener();
         returnUserButtonListener();
+    }
+
+    private void initiateVars(){
+        currentUser = (UserApplication) getApplicationContext();
+        usernameText = findViewById(R.id.usernameText);
+        cellphoneText = findViewById(R.id.cellpjoneUserText);
+    }
+
+    private void renderUsernameInfo(){
+        usernameText.setText(currentUser.user.getUserName());
+        cellphoneText.setText(currentUser.user.getCellphone());
     }
 
     private void logoutButtonListener(){
